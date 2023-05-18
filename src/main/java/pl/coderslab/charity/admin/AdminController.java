a9f0e8371c0860900b5564001cde924f603c1f13
Adam <adamstepnicky@gmail.com>
2023-05-18 12:43:44 +0200

    Display institutions in admin dashboard

--- src/main/java/pl/coderslab/charity/admin/AdminController.java
+++ src/main/java/pl/coderslab/charity/admin/AdminController.java
@@ -6,13 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import pl.coderslab.charity.institution.Institution;
+import pl.coderslab.charity.institution.InstitutionService;
 import pl.coderslab.charity.user.CurrentUser;
 import pl.coderslab.charity.user.User;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 @SessionAttributes("user")
 public class AdminController {
+
+    private final InstitutionService institutionService;
+
+    public AdminController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
 
     @GetMapping("")
     public String adminDashboard(@AuthenticationPrincipal CurrentUser currentUser,
@@ -20,5 +30,12 @@ public class AdminController {
         User user = currentUser.getUser();
         model.addAttribute("user", user);
         return "admin/dashboard";
+    }
+
+    @GetMapping("/institution/list")
+    public String institutionList(Model model) {
+        List<Institution> institutions = institutionService.getAllInstitutions();
+        model.addAttribute("institutions", institutions);
+        return "admin/institution-list";
     }
 }
--- src/main/webapp/WEB-INF/views/admin/dashboard.jsp
+++ src/main/webapp/WEB-INF/views/admin/dashboard.jsp
@@ -37,7 +37,7 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Admin dashboard</h1>
+          <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
 
         </div>
         <!-- /.container-fluid -->
